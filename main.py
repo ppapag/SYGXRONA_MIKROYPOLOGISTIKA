647247d3e4e32263f1e4592b6c73bc3d0908e2c6
ppapag <ppapag@uniwa.gr>
2025-11-18 12:05:38 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,33 @@
+def on_forever():
+    basic.show_leds("""
+        # . . . .
+        . # . . .
+        . . # . .
+        . . . # .
+        . . . . #
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . #
+        . . . # .
+        . . # . .
+        . # . . .
+        # . . . .
+        """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        . . . . .
+        """)
+basic.forever(on_forever)
