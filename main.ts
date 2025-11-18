981a7c03f52d0bc48d75505a8d213e6fb0b48bd8
ppapag <ppapag@uniwa.gr>
2025-11-18 12:25:14 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,18 @@
+let s = 0
+
 basic.forever(function () {
-    basic.showLeds(`
-        # . . . .
-        . # . . .
-        . . # . .
-        . . . # .
-        . . . . #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . # . .
-        . . # . .
-        . . # . .
-        . . # . .
-        . . # . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . #
-        . . . # .
-        . . # . .
-        . # . . .
-        # . . . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # # # # #
-        . . . . .
-        . . . . .
-        `)
+    // draw and erase in one pass
+    for (let i = 0; i < 5; i++) {
+        led.unplot(i, i)
+        led.unplot(4 - i, i)
+        led.unplot(2, i)
+        led.unplot(i, 2)
+    }
+
+    if (s == 0) for (let i = 0; i < 5; i++) led.plot(i, i)
+    if (s == 1) for (let i = 0; i < 5; i++) led.plot(2, i)
+    if (s == 2) for (let i = 0; i < 5; i++) led.plot(4 - i, i)
+    if (s == 3) for (let i = 0; i < 5; i++) led.plot(i, 2)
+
+    s = (s + 1) % 4
 })
