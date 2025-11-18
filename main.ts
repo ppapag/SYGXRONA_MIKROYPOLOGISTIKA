9f7842f3fd3efa4d65acc71607e4ffaf1eb0474c
ppapag <ppapag@uniwa.gr>
2025-11-18 12:44:18 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,12 @@
-let s = 0
-
-basic.forever(function () {
-    // draw and erase in one pass
-    for (let i = 0; i < 5; i++) {
-        led.unplot(i, i)
-        led.unplot(4 - i, i)
-        led.unplot(2, i)
-        led.unplot(i, 2)
-    }
-
-    if (s == 0) for (let i = 0; i < 5; i++) led.plot(i, i)
-    if (s == 1) for (let i = 0; i < 5; i++) led.plot(2, i)
-    if (s == 2) for (let i = 0; i < 5; i++) led.plot(4 - i, i)
-    if (s == 3) for (let i = 0; i < 5; i++) led.plot(i, 2)
-
-    s = (s + 1) % 4
-})
+function forever_(body: () => void) {
+    control.inBackground(() => {
+        while (true) {
+            body();
+            basic.pause(20);
+        }
+    });
+}
+forever_(() => {
+    led.toggle(0, 0); // Toggle the LED at (0,0) on the micro:bit
+    basic.showNumber(input.temperature()); // Display the temperature
+});
